ac3322814bc38664c8f7382d28b208d9bf8c757f
Jakub Cybulski <jakub_cybulski@o2.pl>
2026-06-26 13:24:34 +0200

    Init project

---
+++ src/test/java/p/projects/springbootbookstore/SpringBootBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package p.projects.springbootbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBookStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
